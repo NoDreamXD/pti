3e5d845bf7da727d61b4421f72aba73fc7833966
Егорова Ирина Сергеевна <EIS.19@free.uni-dubna.ru>
2022-04-05 14:19:16 +0300

    wip: task5

--- task5/script.js
+++ task5/script.js
@@ -1,1 +1,24 @@
+const ids = [
+	"city",
+	"tour",
+	"inet",
+	"real",
+	"edu",
+	"bus",
+]
 
+ids.forEach((id, index, arr) => {
+	const el = document.getElementById(id);
+
+	el.onmouseover = () => {
+		el.src = `panel/${id}1.gif`;
+	}
+	el.onmouseout = () => {
+		el.src = `panel/${id}.gif`;
+	}
+})
+
+puppies.onchange = () => {
+	const puppy = puppies.options[puppies.selectedIndex].value;
+	puppyImage.src = `assets/images/img${puppy}.jpg`
+}
