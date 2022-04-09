ad9b7e2198fb614a7ba6649ad6bb834a0a993517
NoDreamXD <egorovarina002@gmail.com>
2022-04-09 18:09:08 +0300

    feat: complete task5

--- task5/script.js
+++ task5/script.js
@@ -1,24 +1,25 @@
-const ids = [
-	"city",
-	"tour",
-	"inet",
-	"real",
-	"edu",
-	"bus",
-]
+const ids = ["city", "tour", "inet", "real", "edu", "bus"];
 
-ids.forEach((id, index, arr) => {
-	const el = document.getElementById(id);
+ids.forEach((id) => {
+  const el = document.getElementById(id);
 
-	el.onmouseover = () => {
-		el.src = `panel/${id}1.gif`;
-	}
-	el.onmouseout = () => {
-		el.src = `panel/${id}.gif`;
-	}
-})
+  el.onmouseover = () => {
+    el.src = `panel/${id}1.gif`;
+  };
+  el.onmouseout = () => {
+    el.src = `panel/${id}.gif`;
+  };
+});
 
+puppies.selectedIndex = 0;
 puppies.onchange = () => {
-	const puppy = puppies.options[puppies.selectedIndex].value;
-	puppyImage.src = `assets/images/img${puppy}.jpg`
+  const puppy = puppies.options[puppies.selectedIndex].value;
+  puppyImage.src = `assets/images/img${puppy}.jpg`;
+};
+
+let currentPuppyImageIndex = 1;
+function nextSliderImage() {
+  currentPuppyImageIndex = (currentPuppyImageIndex % 6) + 1;
+  puppyImageNew.src = `assets/images/img${currentPuppyImageIndex}.jpg`;
 }
+setInterval(nextSliderImage, 2000);
