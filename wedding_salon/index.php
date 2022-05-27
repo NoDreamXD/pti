714dc79ed311734b7b4bc51f5e4dca1090aa56d3
NoDreamXD <egorovarina002@gmail.com>
2022-05-27 19:21:02 +0300

    fix project

--- wedding_salon/index.php
+++ wedding_salon/index.php
@@ -9,11 +9,12 @@
 <body>
     <?php
     $link = mysqli_connect("localhost", "root", '') or die("Could not connect to MySQL server!"); // устанавливаем соединение с mySQL server
-  mysqli_select_db($link, "newdb") or die("Could not select database!"); // указываем с какой БД планируется работать
+    echo $link;
+    mysqli_select_db($link, "newdb") or die("Could not select database!"); // указываем с какой БД планируется работать
 
   if (isset($_POST['write'])) {
     //записываем данные из полей формы в БД
-    $sql = "INSERT INTO newtable (name, email, phone_number, address, letter_theme, message) VALUES ('{$_POST["Name"]}', '{$_POST["Email"]}', '{$_POST["Phone_number"]}', '{$_POST["Address"]}', '{$_POST["Letter_theme"]}', '{$_POST["Message"]}')";
+    $sql = "INSERT INTO wedding_salon_messages (name, email, phone_number, address, letter_theme, message) VALUES ('{$_POST["Name"]}', '{$_POST["Email"]}', '{$_POST["Phone_number"]}', '{$_POST["Address"]}', '{$_POST["Letter_theme"]}', '{$_POST["Message"]}')";
     if (mysqli_query($link, $sql)) {
       echo "Отправлено!" . "<br />";
     } else {
