5a92598fb58b2be303a1387257871294154e1ea5
NoDreamXD <egorovarina002@gmail.com>
2022-05-27 19:20:40 +0300

    fix task9

--- task9/script.js
+++ task9/script.js
@@ -1,27 +1,35 @@
+//function for item
 function dragStart(ev) {
+  console.log('start')
   ev.dataTransfer.effectAllowed = "link";
   ev.dataTransfer.setData("Text", ev.target.getAttribute("id"));
   ev.dataTransfer.setDragImage(ev.target, 0, 0);
   return true;
 }
 
+function dragEnd(ev) {
+  console.log('end')
+  ev.dataTransfer.clearData("Text");
+  return true;
+}
+
+// function for container
+//
 function dragEnter(ev) {
+  console.log('enter')
   var idelt = ev.dataTransfer.getData("Text");
   return true;
 }
 
 function dragOver(ev) {
+  console.log('over')
   var idelt = ev.dataTransfer.getData("Text");
   var id = ev.target.getAttribute("id");
   return false;
 }
 
-function dragEnd(ev) {
-  ev.dataTransfer.clearData("Text");
-  return true;
-}
-
 function dragDrop(ev) {
+  console.log('drop')
   var idelt = ev.dataTransfer.getData("Text");
   ev.target.appendChild(document.getElementById(idelt));
   ev.stopPropagation();
